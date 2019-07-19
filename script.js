9b0b8b526e84a9ff4b53bd332d4eed0aea1c4dc6
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-07-19 16:24:42 +0200

    animeJS intro

--- script.js
+++ script.js
@@ -1,89 +1,37 @@
-/**************************
- 
-  VANILLA JS
+const btn = document.querySelector('#btn');
+const btn2 = document.querySelector('#btn2');
+const section1 = document.querySelector('.section-1');
+const section2 = document.querySelector('.section-2');
+const overlay = document.querySelector('.morph');
 
-***************************/
-
-// let divs = document.querySelectorAll('div');
-
-// let num = 0;
-
-// const delay = time => new Promise(resolve => setTimeout(resolve, time));
-
-// // ES5 Promise => then
-// divs.forEach(div => {
-//   delay(0).then(() => {
-//     setTimeout(() => {
-//       div.style.backgroundColor = 'lightblue';
-//     }, (num += 100));
-//   });
-//   delay(500).then(() => {
-//     setTimeout(() => {
-//       div.style.backgroundColor = '#A0D5FF';
-//       div.style.width = '90%';
-//     }, (num += 100));
-//   });
-// });
-
-// // ES6 async await
-// divs.forEach(async div => {
-//   await delay(0);
-//   setTimeout(() => {
-//     div.style.backgroundColor = 'lightblue';
-//   }, (num += 100));
-
-//   await delay(500);
-//   setTimeout(() => {
-//     div.style.backgroundColor = '#A0D5FF';
-//     div.style.width = '90%';
-//   }, (num += 100));
-// });
-
-/**************************
- 
-  ANIME JS
-
-***************************/
-
-// create timeline
-let tl = anime.timeline({
-  easing: 'easeOutExpo',
-  duration: 750
-});
-
-tl.add({
-  targets: 'div',
-  width: '100%',
-  backgroundColor: 'rgb(197, 197, 255)',
-  delay: anime.stagger(100) // function works only with latest version 3.0.1
-});
-
-tl.add({
-  targets: 'div',
-  width: '90%',
-  backgroundColor: 'rgb(235, 235, 255)'
-});
-
-tl.add(
-  {
-    targets: 'h1',
-    top: '10%',
-    opacity: 1,
-    duration: 4000
-  },
-  '-=1600'
-);
-
-let rotateOnMouseOver = anime({
-  targets: 'section',
-  scaleY: '2',
-  scaleX: '2',
-  translateX: '40%',
-  rotate: '45deg',
-  duration: 5000,
+const morphing = anime({
+  targets: '.morph',
+  d: [
+    {
+      value:
+        'M0,0H0S177.589-3.419,435.908,0C728.248,0,675.25-1.71,998.363,0s364.143-1.71,608.614,0,312.892,0,312.892,0H0Z'
+    },
+    {
+      value:
+        'M0,0V1079.819s94.027-1.71,429.107,0c227.375,3.418,211.971,0,564.164,0s340.379-1.709,569.293,0,357.3,0,357.3,0V0Z'
+    }
+  ],
+  easing: 'easeInOutQuint',
+  duration: 1300,
+  loop: false,
   autoplay: false
 });
 
-document.querySelector('h1').addEventListener('mouseover', () => {
-  rotateOnMouseOver.play();
+btn.addEventListener('click', () => {
+  morphing.restart();
+  overlay.classList.add('pointer');
+  section2.classList.add('animated', 'display', 'flipInY', 'delay-1s');
+});
+
+btn2.addEventListener('click', () => {
+  morphing.reverse();
+  morphing.play();
+
+  overlay.classList.remove('pointer');
+  section2.classList.remove('animated', 'display', 'flipInY', 'delay-1s');
 });
